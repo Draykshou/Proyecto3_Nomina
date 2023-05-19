d7d44f2a699a16871d8a469d877fa409dc662212
Draykshou <diegovco22@gmail.com>
2023-05-18 22:57:47 -0700

    ProyectoU3POO_Nomina_8

--- Nomina/Nomina/Menu.java
+++ Nomina/Nomina/Menu.java
@@ -6,36 +6,44 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class Menu {    
     private static ArrayList<Empleado> empleados = new ArrayList<Empleado>();
+    private static ArrayList<String> nominas = new ArrayList<String>();
     private static Random rnd = new Random();
     private static Scanner sc = new Scanner(System.in);
    
     public static void main(String[]args){
         inicializarListaEmpleados(empleados);
         inicializarEmpladosTemporales(empleados, rnd.nextInt(74)+25);
-        String opcion;
+       String opcion;
         do {
             limpiarConsola();
             opcion = elegirOpcion();
             switch(opcion){
-                case "1": // Imprimir Nomina
+            case "1": // Imprimir Nomina
 				imprimirNomina(empleados);
 				pausarPrograma();
 				break;
-			case "2": // Ver empleado
+            case "2":
+                sigQuincena(empleados);
+                imprimirNomina(empleados);
+                pausarPrograma();
+                break;
+			case "3": // Ver empleado
 				imprimirNomina(empleados);
 				verEmpleado();
 				pausarPrograma();
 				break;
-			case "3": // Modificar empleado
+			case "4": // Modificar empleado
                 imprimirNomina(empleados);
 				modificarFaltasEmpleado();
 				pausarPrograma();
 				break;
-            case "4":
-                System.out.println(ImprimirTablaDivisas(CantidadBilletes(empleados)));
+            case "5":
+                ArrayList<Empleado> temporales = CrearListaEmpledosTemporales(empleados);
+                System.out.println(ImprimirTablaDivisas(CantidadBilletes(temporales), temporales));
+
                 pausarPrograma();
                 break;
-            case "5": // Salir
+            case "6": // Salir
                     System.out.println("Saliendo del programa.");
                     break;
 			default: // entrada invalida
@@ -43,16 +51,17 @@ public class Menu {
 				pausarPrograma();
 				break;
 			}
-		} while (!opcion.equals("4"));
+		} while (!opcion.equals("6"));
 	}
     
     public static String elegirOpcion(){
         System.out.println("¿Que desea realizar?\n");
         System.out.println("1. Imprimir la nomina de empleados.");
-        System.out.println("2. Ver Empleado.");
-        System.out.println("3. Modificar Empleado.");
-        System.out.println("4. Cantidad de dinero para pagar a empleados temporales.\n");
-        System.out.println("5. Salir del programa.\n");
+        System.out.println("2. Siguiente quincena.");
+        System.out.println("3. Ver Empleado.");
+        System.out.println("4. Modificar Empleado.");
+        System.out.println("5. Cantidad de dinero para pagar a empleados temporales.");
+        System.out.println("6. Salir del programa.\n");
         return sc.nextLine().trim();
     }
 
@@ -61,10 +70,32 @@ public class Menu {
         for(int i = 0; i < e.size(); i++){
             salida += e.get(i).ImprimirDatosNomina();
         }
+        nominas.add(salida); // guardar todas las nominas
         System.out.println("\n---------------------------------------------------------------------------------------------------------------------------");
         System.out.print(salida);
         System.out.println("---------------------------------------------------------------------------------------------------------------------------");
     }
+    public static void sigQuincena(ArrayList<Empleado> e){
+    	
+    	for (int i=0; i<e.size(); i++) {
+			if (e.get(i) instanceof EmpleadoBase) {
+				EmpleadoBase bases = (EmpleadoBase) e.get(i);
+				bases.setNumFaltas(rnd.nextInt(3));
+			}
+			if (e.get(i) instanceof EmpleadoComision) {
+				EmpleadoComision comisionados = (EmpleadoComision) e.get(i);
+				comisionados.setCantidadVendida(rnd.nextInt(40001)+20000);
+			}
+			if (e.get(i) instanceof EmpleadoDia) {
+				EmpleadoDia dias = (EmpleadoDia) e.get(i);
+				dias.setDiasTrabajados(rnd.nextInt(4)+10);
+			}
+			if (e.get(i) instanceof EmpleadoHora) {
+				EmpleadoHora horas = (EmpleadoHora) e.get(i);
+				horas.setHorasTrabajadas(rnd.nextInt(41)+20);
+			}	
+			}
+    	}
     
     public static void verEmpleado(){
         System.out.println("Ingrese el numero de empleado: ");
@@ -154,6 +185,7 @@ public class Menu {
         return e;
     }
     
+    // -------------------------------------Cantidad billetes--------------------------------------------------
     public static float calcularTotalPagar(ArrayList<Empleado> em){
         float totalSueldoEmpleados = 0;
         for (Empleado e : em) {
@@ -165,35 +197,57 @@ public class Menu {
         return Math.round(totalSueldoEmpleados * 100) / 100f;
     }
 
-    public static int [] CantidadBilletes(ArrayList<Empleado> em){
-        int[] denominaciones = {500, 200, 100, 50, 20, 10, 5, 2, 1};
-        int[] cantidadDenominaciones = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-        for (Empleado e : em) {
-            float sueldoE = e.sueldo();
+    public static ArrayList<Empleado> CrearListaEmpledosTemporales(ArrayList<Empleado> lista){
+        ArrayList<Empleado> temporales = new ArrayList<>();
+        for(Empleado e: lista){
             if(!(e instanceof EmpleadoBase)){
-                 for (int i = 0; i < denominaciones.length; i++) {
-                    cantidadDenominaciones [i] += (int)sueldoE / denominaciones[i];
-                    sueldoE %= denominaciones[i];
-                 }
+                temporales.add(e);
             }
+        }
+        return temporales;
+    }
+
+    public static int [][] CantidadBilletes(ArrayList<Empleado> e){
+        int[] denominaciones = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+        int[][] cantidadDenominaciones = new int[e.size()][denominaciones.length];
+
+        for (int i = 0; i < cantidadDenominaciones.length; i++) {
+            for (int j = 0; j < cantidadDenominaciones[0].length; j++) {
+                cantidadDenominaciones[i][j] = 0;
+            }
+        }
+        
+        for (int i = 0; i < cantidadDenominaciones.length; i++) {
+            int sueldoE = (int)e.get(i).sueldo();
+                 for (int j = 0; j < cantidadDenominaciones[0].length; j++) {
+                    cantidadDenominaciones[i][j] += sueldoE / denominaciones[j];
+                    sueldoE %= denominaciones[j];
+                 }
         }
         return cantidadDenominaciones;
     }
+    
+    public static String ImprimirTablaDivisas(int [][] divisas, ArrayList<Empleado> e){
+        String salida = String.format("| %-14s | %-8s | %-8s | %-8s | %-8s | %-8s | %-8s | %-8s | %-8s | %-8s | %-10s | %-12s |%n","N° Empleado", "500", "200", "100", "50", "20", "10", "5", "2", "1", "Centavos", "Total:");
+        
+        for (int i = 0; i < divisas.length; i++) {
+                salida += String.format("| %-14s |", e.get(i).getNumEmpleado());
+                for (int j = 0; j < divisas[0].length + 1; j++) {
+                    if(j < divisas[0].length){
+                        salida += String.format( " %-8s |", divisas[i][j]);
+                    }
+                    else{
+                        salida += String.format( " %-10s |", Math.round((e.get(i).sueldo() - (int)e.get(i).sueldo()) * 100) / 100f);
+                    }
+                }
+                salida += String.format(" %-12s |%n", e.get(i).sueldo());
+            }
+        // int totalDenominaciones = 0;
+        // for (int i = 0; i < denominaciones.length; i++) {
+        //     totalDenominaciones += denominaciones[i];
+        // }
 
-    public static String ImprimirTablaDivisas(int [] divisas){
-        String salida = String.format("| %-14s | %-14s | %-14s |%n","Denominacion", "Cantidad", "Total");
-        int[] denominaciones = {500, 200, 100, 50, 20, 10, 5, 2, 1};
-        for(int i = 0; i < denominaciones.length; i++){
-            salida += String.format("| %-14s | %-14s | %-14s |%n", denominaciones[i], divisas[i], denominaciones[i] * divisas[i]);
-        }
-
-        int totalDenominaciones = 0;
-        for (int i = 0; i < denominaciones.length; i++) {
-            totalDenominaciones += denominaciones[i];
-        }
-
-        salida += String.format("| %-14s | %-14s | %-14s |%n", "", totalDenominaciones, (int)calcularTotalPagar(empleados));
+        // salida += String.format("| %-14s | %-14s | %-14s |%n", "", totalDenominaciones, (int)calcularTotalPagar(empleados));
         return salida;
     }
 
@@ -209,3 +263,4 @@ public class Menu {
     }
 
 }
+// quiero pene quiero pene
